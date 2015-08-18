c7cc3c056aeb865f05a107a570e96524ac074deb
andysm1th22 <andrewsmiththefirst@gmail.com>
2015-08-18 16:00:19 -0400

    Working Files
    JSON files generated from Matlab data, passed through javascript through
    Jquery, uploaded and styled onto web pages.

---
+++ datasite/dataStream3.m
@@ -0,0 +1,2 @@
+data=struct('title','data stream','stream',12094129048120948102948102948109480192438295723,'image','chipotle.jpg');
+savejson('dataStream',data,'C:\Users\asmith\Desktop\datasite\dataStream3.json')
